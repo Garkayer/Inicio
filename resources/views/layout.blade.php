edb81e88a654ca30fdf369aa84670d3266a6fd46
Carlos <demonicinha@gmail.com>
2021-06-14 17:10:15 -0600

    Nuevo cambio

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -14,8 +13,6 @@
     <!-- Styles -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-
-        <link href="/css/estilos.css" rel="stylesheet">
 
 </head>
 <body>
@@ -50,7 +47,7 @@
                     </ul>
 
                      <!-- Right Side Of Navbar -->
-                     <ul class="navbar navbar-light bg-light">
+                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">

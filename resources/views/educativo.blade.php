cd19cd4d3dde5b92d2d4127cef39907b2c128494
Carlos <demonicinha@gmail.com>
2021-06-14 19:26:57 -0600

    Casi finalizando

--- resources/views/educativo.blade.php
+++ resources/views/educativo.blade.php
@@ -1,10 +1,32 @@
 @extends('layout')
 
+    
 @section('contenidos')
-<center>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/6RACWyJJPr8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-</center>
 
-<iframe width="560" height="315" src="https://www.youtube.com/embed/YiHTNfKJwAw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
+
+
+<iframe width="450" height="315" allowfullscreen
+src="https://youtube.com/embed/6RACWyJJPr8"> <!-- put your video ID after embed/ -->
+</iframe>
+
+<iframe width="450" height="315" allowfullscreen
+src="https://www.youtube.com/embed/cvakvfXj0KE"> <!-- put your video ID after embed/ -->
+</iframe>
+
+<iframe width="450" height="315" allowfullscreen
+src="https://www.youtube.com/embed/YiHTNfKJwAw"> <!-- put your video ID after embed/ -->
+</iframe>
+
+<iframe width="450" height="315" allowfullscreen
+src="https://www.youtube.com/embed/9MlKHAhuXlo"> <!-- put your video ID after embed/ -->
+</iframe>
+
+<iframe width="450" height="315" allowfullscreen
+src="https://www.youtube.com/embed/xCaI-5WVRlY"> <!-- put your video ID after embed/ -->
+</iframe>
+
+<iframe width="450" height="315" allowfullscreen
+src="https://www.youtube.com/embed/GlGycs7YJO8"> <!-- put your video ID after embed/ -->
+</iframe>
 @endsection
